911a7f2d7d989ffb37cb2fc7e8335b7226dc7c0c
devashree <devashreemaity@gmail.com>
2024-09-02 19:36:38 +0530

    Add files via upload

---
+++ Pattern_43.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Pattern_43{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter no ");
+        int n=sc.nextInt();
+        for(int i=1;i<n;i++){
+        
+            for(int k=1;k<n-i+1;k++){
+            
+        System.out.print(" ");
+        }
+         for(int j=1;j<=2*i-1;j++){
+ System.out.print(j);
+        }
+        System.out.println();
+        }
+        for(int i=1;i<=n;i++){
+        
+            for(int k=1;k<i;k++){
+            
+        System.out.print(" ");
+        }
+         for(int j=1;j<=2*(n-i)+1;j++){
+ System.out.print(j);
+        }
+        System.out.println();
+        }
+    }
+}   
